dda0f9435c8859cc1da87e8916d86dcacb8557b4
itsprithvi <prithvi.myk@gmail.com>
2023-09-15 11:14:35 +0530

    added redis, and implemented shorten file

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -3,15 +3,43 @@ package routes
 import "time"
 
 type request struct {
-	URL         		string				`json:"url"`
-	CustomShort 		string				`json:"short`
-	Expiry      		time.Duration		`json:"expiry`
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short`
+	Expiry      time.Duration `json:"expiry`
 }
 
 type response struct {
-	URL					string				`json:"url"`
-	CustomShort			string				`json:"short"`
-	Expiry				time.Duration		`json:"expiry"`
-	XRateRemaining		int					`json:"rate_limit"`
-	XRateLimitReset		time.Duration		`json:"rate_limit_reset"`
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse JSON"})
+	}
+
+	//implement rate limiting 
+
+
+	//check if the input , if an actual URL
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid URL"})
+	}
+	
+	// check for domain error
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error":"you can't access"})
+	}
+	
+	//enforce https, SSL
+
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
